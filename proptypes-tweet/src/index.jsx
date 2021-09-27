ca0c0781387cf48a5c6ea16481d4c3b9cbca6a9f
Ivan Esmantovich <ivanesmantovich@gmail.com>
2021-09-27 17:07:00 +0300

    Almost finished Chapter 8

---
+++ proptypes-tweet/src/index.jsx
@@ -0,0 +1,133 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import moment from 'moment';
+import PropTypes from 'prop-types';
+
+function Avatar({ hash }) {
+	return <img className="avatar" src={hash} alt="avatar" />;
+}
+
+Avatar.propTypes = {
+	hash: PropTypes.string,
+};
+
+function Message({ text }) {
+	return <div className="message">{text}</div>;
+}
+
+Message.propTypes = {
+	text: PropTypes.string,
+};
+
+function Author({ authorData }) {
+	return (
+		<span className="author">
+			<span className="name">{authorData.name}</span>
+			<span className="handle">@{authorData.handle}</span>
+		</span>
+	);
+}
+
+Author.propTypes = {
+	authorData: PropTypes.shape({
+		name: PropTypes.string.isRequired,
+		handle: PropTypes.string.isRequired,
+	}).isRequired,
+};
+
+const Time = ({ time }) => (
+	<span className="time">{moment(time).fromNow()}</span>
+);
+
+Time.propTypes = {
+	time: PropTypes.string,
+};
+
+const ReplyButton = () => <i className="fas fa-reply reply-button" />;
+
+function RetweetCount({ count }) {
+	if (count > 0) {
+		return <span className="retweet-count">{count}</span>;
+	} else {
+		return null;
+	}
+}
+
+const RetweetButton = ({ count }) => (
+	<span className="retweet-button">
+		<i className="fas fa-retweet" /> <RetweetCount count={count} />
+	</span>
+);
+
+RetweetButton.propTypes = {
+	count: PropTypes.number,
+};
+
+const LikeButton = ({ count }) => (
+	<span className="like-button">
+		<i className="fas fa-heart" />
+		{count ? <span className="like-count">{count}</span> : null}
+	</span>
+);
+
+LikeButton.propTypes = {
+	count: PropTypes.number,
+};
+
+const MoreOptionsButton = () => (
+	<i className="fas fa-ellipsis-h more-options-button" />
+);
+
+function Tweet({ tweet }) {
+	return (
+		<div className="tweet">
+			<Avatar hash={tweet.avatar} />
+			<div className="content">
+				<Author authorData={tweet.author} />
+				<Time time={tweet.timestamp} />
+				<Message text={tweet.message} />
+				<div className="buttons">
+					<ReplyButton />
+					<RetweetButton count={tweet.retweets} />
+					<LikeButton count={tweet.likes} />
+					<MoreOptionsButton />
+				</div>
+			</div>
+		</div>
+	);
+}
+
+// Не нужно! (DRY)
+// В компонентах-детях уже все прописано
+// Tweet.propTypes = {
+// 	tweet: PropTypes.shape({
+// 		message: PropTypes.string.isRequired,
+// 		avatar: PropTypes.string.isRequired,
+// 		author: PropTypes.shape({
+// 			handle: PropTypes.string.isRequired,
+// 			name: PropTypes.string.isRequired,
+// 		}).isRequired,
+// 		likes: PropTypes.number,
+// 		retweets: PropTypes.number,
+// 		timestamp: PropTypes.string.isRequired,
+// 	}).isRequired,
+// }; 
+
+Tweet.propTypes = {
+	tweet: PropTypes.object
+}
+
+const testTweet = {
+	message: 'Learning React right now.',
+	avatar: 'scientist.jpg',
+	author: {
+		handle: 'somerandomscientist',
+		name: 'hotscientist',
+	},
+	likes: 9000,
+	retweets: 67,
+	timestamp: '2016-07-30 21:24:37',
+};
+
+ReactDOM.render(<Tweet tweet={testTweet} />, document.querySelector('#root'));
